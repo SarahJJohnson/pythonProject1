fe3c7f7d63b0a984c777d25dc2a0e6cc9bd47c1e
sjj638 <sarah.johnson@newcastele.edu.au>
2021-05-09 12:56:22 +1000

    commit one

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm 10')
-    print_hi('Extra 2')
+    print_hi('Extra 22')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
